a07e2f2e35a83292869388d1ac5bc62b12497ce4
Julia <julia@8thsq.co>
2015-04-29 00:02:42 -0400

    Update rakefile

--- publish.sh
+++ publish.sh
@@ -4,8 +4,13 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
+<<<<<<< Updated upstream
 cd /Users/Home/Dropbox/!jekyll/_site
 /Users/Home/Dropbox/!jekyll/git pull
+=======
+cd /path/to/jekyll/site/
+/path/to/git/git pull
+>>>>>>> Stashed changes
 
 source /Users/jaredwolff/.rvm/environments/ruby-1.9.3-p429
 rake deploy
